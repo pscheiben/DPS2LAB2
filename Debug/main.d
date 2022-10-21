1dcf147ff45542013cea52a0e67874cb010995a3
Peter Scheibenhoffer <peter.scheibenhoffer@gmail.com>
2022-10-22 00:01:05 +0100

    Implemented LAB2 base code on FR5994

---
+++ Debug/main.d
@@ -0,0 +1,24 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /opt/ti/ccs1120/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /opt/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr5994.h
+main.obj: /opt/ti/ccs1120/ccs/ccs_base/msp430/include/in430.h
+main.obj: /opt/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: /opt/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /opt/ti/ccs1120/ccs/ccs_base/msp430/include/legacy.h
+
+../main.c:
+
+/opt/ti/ccs1120/ccs/ccs_base/msp430/include/msp430.h:
+
+/opt/ti/ccs1120/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+/opt/ti/ccs1120/ccs/ccs_base/msp430/include/in430.h:
+
+/opt/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+/opt/ti/ccs1120/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+/opt/ti/ccs1120/ccs/ccs_base/msp430/include/legacy.h:
+
